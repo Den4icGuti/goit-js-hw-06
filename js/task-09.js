a176f3770f8cf6a2b6a72d5f62a4013fa2178891
Den4icGuti <goos7412@gmail.com>
2021-12-07 13:48:20 +0200

    Сделано 9 дз

--- js/task-09.js
+++ js/task-09.js
@@ -1,10 +1,23 @@
 
-const btnRef = document.querySelector('.change-color');
-const spanRef = document.querySelector('.change-color');
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
+const btnRef = document.querySelector('.change-color');
+const spanRef = document.querySelector('.color');
+
+function changeColor() {
+  document.body.style.background = getRandomHexColor();
+};
+
+function spColorCurrent() {
+  spanRef.textContent = getRandomHexColor()
+};
+btnRef.addEventListener('click', changeColor);
+
+btnRef.addEventListener('click',spColorCurrent)
 
 
-btnRef.addEventListener('click', () =>getRandomHexColor())
+
+
